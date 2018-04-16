2453997901372649c404f60e33ae1d864bc3e235
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-04-16 15:02:56 -0300

    21. [PROJETO] Classe ViaCEP - Buscando da Internet o Endereço.

---
+++ Solucao01/App01_ConsultarCEP/App01_ConsultarCEP/Servico/Modelo/Endereco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App01_ConsultarCEP.Servico.Modelo
+{
+    public class Endereco {
+        public int cep { get; set; }
+        public int logradouro { get; set; }
+        public int complemento { get; set; }
+        public int bairro { get; set; }
+        public int localidade { get; set; }
+        public int uf { get; set; }
+        public int unidade { get; set; }
+        public int ibge { get; set; }
+        public int gia { get; set; }
+    }
+}
---
+++ Solucao01/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net; // para uso do WebClient.
+using App01_ConsultarCEP.Servico.Modelo; // para retornar um objeto endereço.
+using Newtonsoft.Json; // deserializador do JSON.
+
+namespace App01_ConsultarCEP.Servico
+{
+    public class ViaCEPServico {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep) {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+            WebClient wc = new WebClient();
+            
+            string conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+        }
+    }
+}
