b7efc261544370e7302f4a66bdd65cb6027a5a1b
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-04-22 13:39:32 -0300

    43. [DEMO] App - Tipos de Página - iOS - Demonstração.

--- Solucao02/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Master/Master.xaml.cs
+++ Solucao02/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Master/Master.xaml.cs
@@ -19,14 +19,17 @@ namespace App02_TipoPaginaXF.TipoPagina.Master
 
         private void MudarPagina1(object sender, EventArgs args) {
             Detail = new NavigationPage(new Navigation.Pagina1());
+            IsPresented = false;
         }
 
         private void MudarPagina2(object sender, EventArgs args) {
             Detail = new Navigation.Pagina2();
+            IsPresented = false;
         }
 
         private void MudarPagina3(object sender, EventArgs args) {
             Detail = new Conteudo();
+            IsPresented = false;
         }
     }
 }
