d9fba2ae6fa5541d523c747ccded52ab302f15df
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-04-22 12:30:32 -0300

    <nav:Pagina1/>.

--- Solucao02/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Carrousel/TipoPagina3.xaml.cs
+++ Solucao02/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Carrousel/TipoPagina3.xaml.cs
@@ -18,7 +18,8 @@ namespace App02_TipoPaginaXF.TipoPagina.Carrousel
 		}
 
         private void MudarPagina(object sender, EventArgs args) {
-            App.Current.MainPage = new NavigationPage(new Navigation.Pagina1()) { BarBackgroundColor = Color.Blue };
+            // App.Current.MainPage = new NavigationPage(new Navigation.Pagina1()) { BarBackgroundColor = Color.Blue };
+            App.Current.MainPage = new Tabbed.Abas();
         }
     }
 }
