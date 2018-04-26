a051e41bb3fc42a3079f6ba63ea85c829318a19e
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-04-26 08:53:28 -0300

    80. [ESTUDO] ListView - C#.

--- Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Controles/ListViewPage.xaml.cs
+++ Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Controles/ListViewPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using App01_ControleXF.Modelo;
 
 namespace App01_ControleXF.Controles
 {
@@ -15,6 +16,15 @@ namespace App01_ControleXF.Controles
 		public ListViewPage ()
 		{
 			InitializeComponent ();
+
+            List<Pessoa> lista = new List<Pessoa>();
+            lista.Add(new Pessoa { Nome = "José", Idade = "20" });
+            lista.Add(new Pessoa { Nome = "Felipe", Idade = "22" });
+            lista.Add(new Pessoa { Nome = "Maria", Idade = "22" });
+            lista.Add(new Pessoa { Nome = "João", Idade = "24" });
+            lista.Add(new Pessoa { Nome = "Paulo", Idade = "26" });
+
+            ListaPessoas.ItemsSource = lista;
 		}
 	}
 }
