9e713819d0cdafba616197f5397f504cb1af2e6a
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-04-29 16:37:03 -0300

    93. [PROJETO] Componente Prioridade - Parte 1 e 2.

--- Seção 08/App2_Tarefa/App2_Tarefa/App2_Tarefa/Telas/Cadastro.xaml.cs
+++ Seção 08/App2_Tarefa/App2_Tarefa/App2_Tarefa/Telas/Cadastro.xaml.cs
@@ -7,14 +7,24 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace App2_Tarefa.Telas
-{
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Cadastro : ContentPage
-	{
-		public Cadastro ()
-		{
-			InitializeComponent ();
-		}
-	}
+namespace App2_Tarefa.Telas {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cadastro : ContentPage {
+        public Cadastro() {
+            InitializeComponent();
+        }
+
+        public void PrioridadeSelectAction(object sender, EventArgs args) {
+            var Stacks = SLPrioridades.Children;
+            foreach (var Linha in Stacks) {
+                Label LblPrioridade = ((StackLayout)Linha).Children[1] as Label;
+                LblPrioridade.TextColor = Color.Gray;
+                ((Label)((StackLayout)sender).Children[1]).TextColor = Color.Black;
+                FileImageSource Source = ((Image)((StackLayout)sender).Children[0]).Source as FileImageSource;
+                String Prioridade = Source.File.ToString().Replace("Resources/", "").Replace(".png", "");
+                txtNome.Text = Prioridade;
+            }
+        }
+
+    }
 }
