e9d480188d6b8b58ac9e8b76ea7daacdbfda2750
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-06 15:01:17 -0300

    112. [DEMO] App - Tipos de Celulas - Correção.

--- Seção 09/App1_Cell/App1_Cell/App1_Cell/Pagina/ImageCellPage.xaml.cs
+++ Seção 09/App1_Cell/App1_Cell/App1_Cell/Pagina/ImageCellPage.xaml.cs
@@ -20,11 +20,11 @@ namespace App1_Cell.Pagina
 
             List<Funcionario> Lista = new List<Funcionario>();
 
-            Lista.Add(new Funcionario() { Foto = "https://media.licdn.com/dms/image/C4E03AQFOggbk903rQA/profile-displayphoto-shrink_200_200/0?e=1529528400&v=beta&t=dJlR2QmHTJJuLm-dVpf9K2zHtcWwOHlKtGRVh4DX_LU", Nome = "José", Cargo = "Presidente da empresa" });
-            Lista.Add(new Funcionario() { Foto = "http://blogs.uai.com.br/cantodogalo/wp-content/uploads/sites/32/2018/04/nathalia_mazzieiro.jpg", Nome = "Maria", Cargo = "Gerente de Vendas" });
-            Lista.Add(new Funcionario() { Foto = "https://ct.yimg.com/cy/4689/38609694164_71b709_128sq.jpg", Nome = "Elaine", Cargo = "Gerente de Marketing" });
-            Lista.Add(new Funcionario() { Foto = "http://0.gravatar.com/avatar/c73a32ea07a8d0f1efc5e010fab5001d?s=128&d=https%3A%2F%2Fwww.midiatismo.com.br%2Fwp-content%2Fthemes%2FMidiatismo%2Fimgs%2Fmystery-logo-150.jpg&r=g", Nome = "Felipe", Cargo = "Entregador" });
-            Lista.Add(new Funcionario() { Foto = "http://www.administradores.com.br/_assets/modules/members/member_273788.jpg?", Nome = "João", Cargo = "Vendedor" });
+            Lista.Add(new Funcionario() { Foto = "perfil.png", Nome = "José", Cargo = "Presidente da empresa" });
+            Lista.Add(new Funcionario() { Foto = "perfil.png", Nome = "Maria", Cargo = "Gerente de Vendas" });
+            Lista.Add(new Funcionario() { Foto = "perfil.png", Nome = "Elaine", Cargo = "Gerente de Marketing" });
+            Lista.Add(new Funcionario() { Foto = "perfil.png", Nome = "Felipe", Cargo = "Entregador" });
+            Lista.Add(new Funcionario() { Foto = "perfil.png", Nome = "João", Cargo = "Vendedor" });
 
             ListaFuncionario.ItemsSource = Lista;
 
