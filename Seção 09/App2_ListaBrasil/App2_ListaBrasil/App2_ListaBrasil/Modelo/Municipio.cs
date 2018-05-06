401e9cd0239549fef639c718944cdefb5f20a5bd
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-06 18:56:30 -0300

    117.[PROJETO] Lista de estados.

---
+++ Seção 09/App2_ListaBrasil/App2_ListaBrasil/App2_ListaBrasil/Modelo/Municipio.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2_ListaBrasil.Modelo {
+    public class Municipio {
+
+    }
+}
---
+++ Seção 09/App2_ListaBrasil/App2_ListaBrasil/App2_ListaBrasil/Municipios.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using App2_ListaBrasil.Modelo;
+
+namespace App2_ListaBrasil
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Municipios : ContentPage
+	{
+		public Municipios (Estado estado)
+		{
+			InitializeComponent ();
+		}
+	}
+}
