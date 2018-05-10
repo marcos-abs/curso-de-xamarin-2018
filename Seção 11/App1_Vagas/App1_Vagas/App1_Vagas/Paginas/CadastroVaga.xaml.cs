a0dae36284e8c2f7c6b57a4faba3d7ed4a9c8147
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-10 20:03:04 -0300

    157. [ESTUDO/PROJETO] C# - Cadastro Vagas.

--- Seção 11/App1_Vagas/App1_Vagas/App1_Vagas/Paginas/CadastroVaga.xaml.cs
+++ Seção 11/App1_Vagas/App1_Vagas/App1_Vagas/Paginas/CadastroVaga.xaml.cs
@@ -6,15 +6,36 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using App1_Vagas.Modelos;
+using App1_Vagas.Banco;
 
-namespace App1_Vagas.Paginas
-{
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CadastroVaga : ContentPage
-	{
-		public CadastroVaga ()
-		{
-			InitializeComponent ();
-		}
-	}
+namespace App1_Vagas.Paginas {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CadastroVaga : ContentPage {
+        public CadastroVaga() {
+            InitializeComponent();
+        }
+
+        public void SalvarAction(object sender, EventArgs args) {
+            
+            Vaga vaga = new Vaga();
+
+            //TODO Validar dados do cadastro
+            vaga.NomeVaga = NomeVaga.Text;
+            vaga.Quantidade = short.Parse(Quantidade.Text);
+            vaga.Salario = double.Parse(Salario.Text);
+            vaga.Empresa = Empresa.Text;
+            vaga.Cidade = Cidade.Text;
+            vaga.Descricao = Descricao.Text;
+            vaga.TipoContratacao = (TipoContratacao.IsToggled) ? "PJ" : "CLT";
+            vaga.Telefone = Telefone.Text;
+            vaga.Email = Email.Text;
+
+            Database database = new Database();
+            database.Cadastro(vaga);
+
+            //TODO Voltar para a tela de pesquisa
+            Navigation.PopAsync();
+        }
+    }
 }
