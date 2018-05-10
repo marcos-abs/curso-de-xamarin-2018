089ce5aa06a0d4d63c0fed61596418daaf2cff9b
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-10 19:45:16 -0300

    156. [ESTUDO/PROJETO] C# - Consulta Vagas.

--- Seção 11/App1_Vagas/App1_Vagas/App1_Vagas/Paginas/ConsultaVagas.xaml.cs
+++ Seção 11/App1_Vagas/App1_Vagas/App1_Vagas/Paginas/ConsultaVagas.xaml.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using App1_Vagas.Modelos;
 
-namespace App1_Vagas.Paginas
-{
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ConsultaVagas : ContentPage
-	{
-		public ConsultaVagas ()
-		{
-			InitializeComponent ();
-		}
-	}
+namespace App1_Vagas.Paginas {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConsultaVagas : ContentPage {
+        public ConsultaVagas() {
+            InitializeComponent();
+        }
+
+        public void GoCadastro(object sender, EventArgs args) {
+            Navigation.PushAsync(new CadastroVaga());
+        }
+
+        public void GoMinhasVagas(object sender, EventArgs args) {
+            Navigation.PushAsync(new MinhasVagasCadastradas());
+        }
+
+        public void MaisDetalhesAction(object sender, EventArgs args) {
+            Label lblDetalhe = (Label)sender;
+            Vaga vaga = ((TapGestureRecognizer)lblDetalhe.GestureRecognizers[0]).CommandParameter as Vaga;
+            Navigation.PushAsync(new DetalheVaga(vaga));
+        }
+    }
 }
--- Seção 11/App1_Vagas/App1_Vagas/App1_Vagas/Paginas/DetalheVaga.xaml.cs
+++ Seção 11/App1_Vagas/App1_Vagas/App1_Vagas/Paginas/DetalheVaga.xaml.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using App1_Vagas.Modelos;
 
 namespace App1_Vagas.Paginas
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DetalheVaga : ContentPage
 	{
-		public DetalheVaga ()
+		public DetalheVaga (Vaga vaga)
 		{
 			InitializeComponent ();
+
+            DisplayAlert("MSG", vaga.NomeVaga, "OK");
 		}
 	}
 }
