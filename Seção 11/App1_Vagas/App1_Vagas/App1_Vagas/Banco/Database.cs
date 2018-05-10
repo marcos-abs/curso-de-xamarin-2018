acd281be97b4c29493fb5cea0fc2361dacd30990
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-09 22:56:54 -0300

    148. [ESTUDO/PROJETO] ICaminho - Android.

---
+++ Seção 11/App1_Vagas/App1_Vagas/App1_Vagas/Banco/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+using App1_Vagas.Modelos;
+using Xamarin.Forms;
+
+namespace App1_Vagas.Banco {
+    class Database {
+        private SQLiteConnection _conexao;
+
+        public Database() {
+            var dep = DependencyService.Get<ICaminho>();
+            _conexao = new SQLiteConnection();
+        }
+
+        public List<Vaga> Consultar() {
+            return null;
+        }
+
+        public Vaga ObterVagaPorID(int id) {
+            return null;
+        }
+
+        public void Cadastro(Vaga vaga) {
+
+        }
+
+        public void Alteracao(Vaga vaga) {
+
+        }
+
+        public void Exclusao(int id) {
+
+        }
+    }
+}
