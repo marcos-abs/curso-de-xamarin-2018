6873979835536450b7779e255843a2bb60e29769
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-11 11:40:23 -0300

    170.[ESTUDO´/PROJETO] Modelo.

---
+++ Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/Model/Grupo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1_Mimica.Model {
+    public class Grupo {
+        public string Nome { get; set; }
+        public short Pontuacao { get; set; }
+    }
+}
---
+++ Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/ResultadoViewModel.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Mimica.ViewModel {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResultadoViewModel : ContentPage {
+        public ResultadoViewModel() {
+            InitializeComponent();
+        }
+    }
+}
