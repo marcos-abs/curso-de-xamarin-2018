9f2be590583768664302580245a59fc917c4b4b6
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-26 20:09:37 -0300

    176. [ESTUDO/PROJETO] Armazenamento - Jogo.

---
+++ Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/Armazenamento/Armazenamento.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using App1_Mimica.Model;
+
+namespace App1_Mimica.Armazenamento {
+    public class Armazenamento {
+        public static Jogo Jogo { get; set; }
+        public static short RodadaAtual { get; set; }
+    }
+}
--- Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/InicioViewModel.cs
+++ Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/InicioViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using App1_Mimica.Model; // para ter acesso as classes modelo do projeto.
 using System.ComponentModel; // para implementar a interface INotifyPropertyChanged.
 using Xamarin.Forms; // para ter acesso ao Tipo Command.
+// using App1_Mimica.Armazenamento; // para ter acesso as classes de armazenamento do jogo. *** DESNECESSARIO PQ A CLASSE E O NAMESPACE TEM O MESMO NOME ***
 
 namespace App1_Mimica.ViewModel {
     public class InicioViewModel : INotifyPropertyChanged {
@@ -17,6 +18,9 @@ namespace App1_Mimica.ViewModel {
         }
 
         private void IniciarJogo() {
+
+            Armazenamento.Armazenamento.Jogo = this.jogo; // sem o uso do using pq utiliza o caminho completo da classe no projeto.
+            Armazenamento.Armazenamento.RodadaAtual = 1;
             App.Current.MainPage = new View.Jogo();
         }
 
