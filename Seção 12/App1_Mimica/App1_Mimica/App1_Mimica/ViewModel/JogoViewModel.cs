d41220c32195118a3c69b98ff2cd4b43784e40f5
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-26 21:06:31 -0300

    177. [ESTUDO/PROJETO] MVVM - Tela Jogo - Binding (Vínculos) na tela.

--- Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/JogoViewModel.cs
+++ Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/JogoViewModel.cs
@@ -2,7 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Xamarin.Forms;
+
 namespace App1_Mimica.ViewModel {
-    class JogoViewModel {
+    public class JogoViewModel {
+        public byte PalavraPontuacao { get; set; }
+        public string Palavra { get; set; }
+        public string TextoContagem { get; set; }
+
+        public bool ContainerContagem { get; set; }
+        public bool ContainerIniciar { get; set; }
+
+        public Command MostrarPalavra { get; set; }
+        public Command Acertou { get; set; }
+        public Command Errou { get; set; }
+        public Command Iniciar { get; set; }
+
+        public JogoViewModel() {
+            ContainerContagem = false;
+            MostrarPalavra = new Command();
+            Acertou = new Command();
+            Errou = new Command();
+            Iniciar = new Command();
+        }
     }
 }
