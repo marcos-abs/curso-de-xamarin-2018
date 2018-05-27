d31d890a77b1ba0f9cd02f6f50ae8f357ff2b706
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-27 12:59:13 -0300

    185. [ESTUDO/PROJETO] MVVM - Tela Jogo - Converter.

---
+++ Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/View/Util/LabelPontuacaoConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App1_Mimica.View.Util {
+    public class LabelPontuacaoConverter : IValueConverter {
+
+        // View > ViewModel (informação)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            //short Pontuacao = short.Parse(value as string); - tipo não é short e sim byte.
+            //return "Pontuação: " + Pontuacao;
+            if((byte) value == 0) {
+                return "Palavra";
+            } else {
+                return "Palavra - Pontuação: " + value;
+            }
+        }
+
+        // ViewModel > View (informação)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
