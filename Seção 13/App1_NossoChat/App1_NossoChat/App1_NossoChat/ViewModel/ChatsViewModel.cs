edc20c3f2271f3c10e165e4fecc7e4f1033d9a16
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-31 10:59:59 -0300

    216. [ESTUDO/PROJETO] - NossoChat - Chat - Mensagem.

--- Seção 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/ViewModel/ChatsViewModel.cs
+++ Seção 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/ViewModel/ChatsViewModel.cs
@@ -11,7 +11,26 @@ using System.Linq; // OrderBy para List<Chat>.
 namespace App1_NossoChat.ViewModel {
     public class ChatsViewModel : INotifyPropertyChanged {
 
-        // Encapsulamento para o OnPropertyChanged funcionar.
+        // Encapsulamento do SelectedItemChat para o OnPropertyChanged funcionar.
+        private Chat _selectedItemChat;
+        public Chat SelectedItemChat {
+            get {
+                return _selectedItemChat;
+            }
+            set {
+                _selectedItemChat = value;
+                OnPropertyChanged("SelectedItemChat");
+                GoPaginaMensagem(value);
+            }
+        }
+
+        private void GoPaginaMensagem(Chat chat) {
+            if(chat != null) {
+                ((NavigationPage)App.Current.MainPage).Navigation.PushAsync(new View.Mensagem(chat));
+            }
+        }
+
+        // Encapsulamento do List<Chat> para o OnPropertyChanged funcionar.
         private List<Chat> _chats;
         public List<Chat> Chats {
             get {
--- Seção 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/ViewModel/MensagemViewModel.cs
+++ Seção 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/ViewModel/MensagemViewModel.cs
@@ -2,7 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 
+using System.ComponentModel; // INotifyPropertyChanged.
+using App1_NossoChat.Model;
+using App1_NossoChat.Service;
+using Xamarin.Forms; // Binding + Command.
+using System.Linq; // OrderBy para List<Chat>.
+
+
 namespace App1_NossoChat.ViewModel {
-    class MensagemViewModel {
+    public class MensagemViewModel : INotifyPropertyChanged {
+        public MensagemViewModel(Chat chat) {
+            //TODO Pesquisa e apresentação na tela.
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Para não ocorrer Exceptions
+        private void OnPropertyChanged(string PropertyName) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
     }
 }
