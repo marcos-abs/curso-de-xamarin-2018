c7ecad53218160b11a5b4e6b48a133cc2c0ca5fd
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-31 15:46:34 -0300

    221. [ESTUDO/PROJETO] NossoChat - Programação - SOAP.

---
+++ Seção 13/App2_SOAP_Client/App2_SOAP_Client/App2_SOAP_Client/IServiceSOAP.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2_SOAP_Client {
+    public interface IServiceSOAP {
+        string Somar(int num1, int num2);
+    }
+}
---
+++ Seção 13/App2_SOAP_Client/App2_SOAP_Client/App2_SOAP_Client/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2_SOAP_Client
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public void EnviarSOAP(object sender, EventArgs args) {
+            var Num1T = int.Parse(Num1.Text);
+            var Num2T = int.Parse(Num2.Text);
+
+            txtResultado.Text = DependencyService.Get<IServiceSOAP>().Somar(Num1T, Num2T);
+
+        }
+    }
+}
