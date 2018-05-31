4d1332e63efaf1caca66b1138da6a05dafd1ae41
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-05-31 10:23:35 -0300

    215. [ESTUDO/PROJETO] NossoChat - Página Cadastro.

--- Seção 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/View/CadastrarChat.xaml.cs
+++ Seção 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/View/CadastrarChat.xaml.cs
@@ -15,6 +15,9 @@ namespace App1_NossoChat.View
 		public CadastrarChat ()
 		{
 			InitializeComponent ();
-		}
+
+            BindingContext = new ViewModel.CadastrarChatViewModel();
+
+        }
     }
 }
--- Seção 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/ViewModel/CadastrarChatViewModel.cs
+++ Seção 13/App1_NossoChat/App1_NossoChat/App1_NossoChat/ViewModel/CadastrarChatViewModel.cs
@@ -2,7 +2,54 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Xamarin.Forms;
+using App1_NossoChat.Model;
+using App1_NossoChat.Service;
+using System.ComponentModel;
+
 namespace App1_NossoChat.ViewModel {
-    class CadastrarChatViewModel {
+    public class CadastrarChatViewModel : INotifyPropertyChanged {
+
+        public string nome { get; set; }
+
+        private string _mensagem { get; set; } 
+        public string mensagem {
+            get { return _mensagem; }
+            set {
+                _mensagem = value;
+                OnPropertyChanged("mensagem");
+            }
+        }
+
+        public Command CadastrarCommand { get; set; }
+
+        public CadastrarChatViewModel() {
+            CadastrarCommand = new Command(Cadastrar);
+        }
+
+        private void Cadastrar() {
+            var chat = new Chat() { nome = nome };
+            bool status = ServiceWS.InsertChat(chat);
+            if (status == true) {
+                ((NavigationPage)App.Current.MainPage).Navigation.PopAsync();
+                var Nav = (NavigationPage)App.Current.MainPage;
+                var Chats = (View.Chats)Nav.CurrentPage;
+                var ViewModel = (ChatsViewModel)Chats.BindingContext;
+                if(ViewModel.AtualizarCommand.CanExecute(null)) {
+                    ViewModel.AtualizarCommand.Execute(null);
+                }
+            } else {
+                mensagem = "Ocorreu um erro no cadastro.";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Para não ocorrer Exceptions
+        private void OnPropertyChanged(string PropertyName) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
     }
 }
